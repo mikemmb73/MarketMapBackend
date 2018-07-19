0e455b5e3efa0ce3f0458e23b38e8a5f811c34b6
Mike <mikemmb73@gmail.com>
2018-07-19 16:47:51 -0700

    added dictionary

--- marketmapbackend/app.js
+++ marketmapbackend/app.js
@@ -37,7 +37,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-app.listen(3000);
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log('Listening on port '+port+'...'));
 
 module.exports = app;
