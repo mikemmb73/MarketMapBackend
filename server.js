59b4668fe74f4a6d1260a92e9d8f4f3c46fd9d08
Mike <mikemmb73@gmail.com>
2018-07-19 12:17:59 -0700

    init commit

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-//set routes
-app.use(require('./app/routes'));
-
-app.listen(3000, function() {
-    console.log('listening on 3000')
-})
